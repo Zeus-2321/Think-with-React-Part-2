d22d636ac215dbd405e48c3aafaf56f33d890c33
Zeus-2321 <109408148+Zeus-2321@users.noreply.github.com>
2022-12-29 10:20:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+class ReactContainer extends React.Component{
+    // JSX tags
+    // JSX there should be one parent element
+      render(){
+          return (
+          <div>Hello! Welcome to Kalvium   
+             <div>This is babel</div>
+         </div> )
+      };  
+  }
+
+const container = document.getElementById('react-container');
+// Directly pass the component to render
+ReactDOM.render(<ReactContainer/>,container); 
+
